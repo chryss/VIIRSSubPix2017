7f94c0a8e46d63061e216fd7f7b288a81a57c607
Chris Waigl <chris.waigl@gmail.com>
2017-10-09 13:11:50 -0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+# configuration for project:
+# VIIRS sub-pixel temperature retrieval
+# 2017-09-22    cwaigl@alaska.edu
+
+viirsdir = '../VIIRS'
+viirscenecatalog = 'viirsgranulecatalog.json'
+
+# only one study area
+maxlat = 67.7
+minlat = 66.9
+maxlon = -140.5
+minlon = -142.0
